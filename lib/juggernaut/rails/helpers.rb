ba6968e9425df66ee7e412d360b6889cf1dd665a
Karl Varga <kjvarga@gmail.com>
2010-04-13 18:52:28 -0700

    Include a juggernaut_options helper so we can instantiate our own Juggernaut instances.

--- lib/juggernaut/rails/helpers.rb
+++ lib/juggernaut/rails/helpers.rb
@@ -2,7 +2,16 @@ module Juggernaut # :nodoc:
   module Rails
     module Helpers
 
-      def juggernaut(options = {})
+      def juggernaut(options = {}, include_tag = true)
+        options = juggernaut_options(options)
+        if include_tag
+          javascript_tag "new Juggernaut(#{options.to_json});"
+        else
+          "new Juggernaut(#{options.to_json});"
+        end
+      end
+      
+      def juggernaut_options(options = {})
         random_host = Juggernaut::Rails.random_host
         options = {
           :host                 => (random_host[:public_host] || random_host[:host]),
@@ -20,7 +29,6 @@ module Juggernaut # :nodoc:
           :reconnect_attempts   => 3,
           :reconnect_intervals  => 3
         }.merge(options)
-        javascript_tag "new Juggernaut(#{options.to_json});"
       end
     end
   end
